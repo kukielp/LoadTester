4ae60953f1ad9962552d82dec828df05fd32fd64
Paul <kukielp@gmail.com>
2017-03-12 20:22:51 +1100

    Conect to mysql with free account

---
+++ LoadTester/Models/ContentSetContext.cs
@@ -0,0 +1,57 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LoadTester.Models
+{
+  public class ContentSetContext
+  {
+    public string ConnectionString { get; set; }
+
+    public ContentSetContext(string connectionString)
+    {
+      this.ConnectionString = connectionString;
+    }
+
+    private MySqlConnection GetConnection()
+    {
+      return new MySqlConnection(ConnectionString);
+    }
+
+    public List<ContentSet> GetAllContentSets()
+    {
+      List<ContentSet> list = new List<ContentSet>();
+
+      using (MySqlConnection conn = GetConnection())
+      {
+        conn.Open();
+        MySqlCommand cmd = new MySqlCommand("SELECT contentSetId, categoryId, name, active FROM contentSet", conn);
+        using (MySqlDataReader reader = cmd.ExecuteReader())
+        {
+          while (reader.Read())
+          {
+            list.Add(new ContentSet()
+            {
+              contentSetId = reader.GetInt32("contentSetId"),
+              categoryId = reader.GetInt32("categoryId"),
+              name = reader.GetString("name"),
+			  active = reader.GetBoolean("active")
+			
+
+            });
+						var contentSetId = reader.GetInt32("contentSetId");
+						var categoryId = reader.GetInt32("categoryId");
+						var name = reader.GetString("name");
+			 			string row = $"{contentSetId}\t\t{categoryId}\t\t{name}";
+                System.Console.WriteLine(row);
+          }
+        }
+      }
+
+      return list;
+    }
+  }
+}
---
+++ LoadTester/Models/ContetSet.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LoadTester.Models
+{
+	public class ContentSet
+	{
+		public int contentSetId { get; set; }
+	 
+	    public int categoryId { get; set; }
+	 
+	    public string name { get; set; }
+	 
+		public bool active { get; set; }
+	}
+}
